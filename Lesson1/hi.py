edc998c6c9b06d7d5e69629a1234986bcbbd4b85
nakshpatelthecoder <25pateln@students.watfordboys.org>
2025-12-02 18:03:23 +0000

    My first python code

---
+++ Lesson1/hi.py
@@ -0,0 +1,8 @@
+#Printing a Greeting Message 
+print("Hello,world")
+
+# Printing my name
+print("My name is Naksh")
+
+#Printing a fact about myself
+print (" I am one of the smartest people in my class")
