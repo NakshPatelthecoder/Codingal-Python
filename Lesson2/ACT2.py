8d06bd34a460206b5c0f1e710fe66f35a4d624e2
nakshpatelthecoder <25pateln@students.watfordboys.org>
2025-12-09 17:36:34 +0000

    ACT2

---
+++ Lesson2/ACT2.py
@@ -0,0 +1,7 @@
+# Creating different variables
+
+name=input (" What is your name? ")
+age=input (" What is your age? ")
+codingal="codingal"
+
+print(" Nice to meet you " , name, "\n Welcome to ", codingal,)
