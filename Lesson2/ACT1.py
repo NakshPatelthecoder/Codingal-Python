157c55a0c1a871c5c91a7cd12c8488195334d68e
nakshpatelthecoder <25pateln@students.watfordboys.org>
2025-12-09 17:22:40 +0000

    Lesson2 ACT1

---
+++ Lesson2/ACT1.py
@@ -0,0 +1,14 @@
+# Printing a sentence 
+print("Welcome to this world of programming. Here you will find out how you can give a computer instructions to operate.")
+
+# Printing a number
+print (8000670)
+
+# Now we are going to print two lines
+print("Hello \n This is the world of programming")
+
+# Now we are going to print multiple arguements at a time
+print(" Hello we are now going to learn hw to print multiple arguments", 678910)
+
+# We are now going to add a special character to the end
+print("Hello hope you are ready for this" , end= "*&$£!%")
