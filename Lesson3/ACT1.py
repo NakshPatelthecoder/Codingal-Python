bf5cd1a5a245930ce93371b4c7efbd6f1e086bda
nakshpatelthecoder <25pateln@students.watfordboys.org>
2025-12-16 17:33:05 +0000

    ACP1.py

---
+++ Lesson3/ACT1.py
@@ -0,0 +1,13 @@
+#Let's check the data types of different values
+a=19
+print("type of a: ",type(a))
+
+
+b=67.5
+print("type of b:", type(b))
+
+c= "Naksh"
+print("type of c:", type(c))
+
+d=True
+print("type of d:", type(d))
