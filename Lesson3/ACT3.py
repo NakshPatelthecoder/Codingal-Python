d98e68b46dc09965950658ea460785b58b47caea
nakshpatelthecoder <25pateln@students.watfordboys.org>
2025-12-16 17:56:43 +0000

    ACT3.py

---
+++ Lesson3/ACT3.py
@@ -0,0 +1,7 @@
+# Input a word
+text=input("Enter a word or a sentence: ")
+
+# Reverse the String
+# Using step Value as -1 to iterate the String in reverse order
+revText=text[::-1]
+print("Reversed String is:",revText)
