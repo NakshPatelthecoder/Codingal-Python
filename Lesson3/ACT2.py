8dffca2f72e8738682ec10d86b266a8c36d9985b
nakshpatelthecoder <25pateln@students.watfordboys.org>
2025-12-16 17:51:22 +0000

    ACT2P2.p

---
+++ Lesson3/ACT2.py
@@ -0,0 +1,34 @@
+# Assigning Different Variables
+name="Naksh Patel"
+age=11
+is_student=True
+weight=39.95
+height=146
+
+# Printing Different Variables and theit respective Data Types
+print("Name:", name)
+print("Data Type is:", type(name))
+print("\n Age:", age)
+print("Data Type is:", type(age))
+print("\n Is Student:", is_student)
+print("Data Type is:", type(is_student))
+print("\n Weight:", weight)
+print("Data Type is:", type(weight))
+print("\n Height:", height)
+print("Data Type is:", type(height))
+
+
+
+# Typecasting Different Variables
+weight=39.95
+
+print(weight)#
+
+# Chaninging float to int
+weight67=int(weight)
+print(weight67)
+
+weight69=str(weight)
+print(weight69)
+
+
