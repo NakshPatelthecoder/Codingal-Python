f3d33637130e2a7b0d490c9f739a2de7410f2478
nakshpatelthecoder <25pateln@students.watfordboys.org>
2025-12-23 17:42:44 +0000

    4.ACT2.py

---
+++ Lesson4/4.ACT1.py
@@ -0,0 +1,13 @@
+# Asking the user to give heights
+print("Please eter the heights of all the trees in inches below.")
+height1=int(input(" Please enter the height of tree 1: "))
+height2=int(input(" Please enter the height of tree 2: "))
+height3=int(input(" Please enter the height of tree 3: ")) 
+height4=int(input(" Please enter the height of tree 4: "))
+height5=int(input(" Please enter the height of tree 5: "))
+
+# Summing the heights of the trees
+sum = height1 + height2 + height3 + height4 + height5
+
+# Averaging th heights 
+print( "The average of the five tress above is: ", sum/5 )
