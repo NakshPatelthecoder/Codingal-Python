8e0d4172b1eede253bc0e1666fb235e49c014974
nakshpatelthecoder <25pateln@students.watfordboys.org>
2025-12-23 17:59:32 +0000

    4.ACT3.py

---
+++ Lesson4/4.ACT3.py
@@ -0,0 +1,9 @@
+# Taking the marks from the user
+print(" Please enter the marks obtained from 4 subjects:")
+Maths = int(input(" Maths:"))
+English = int(input(" English:"))
+Science = int(input(" Science:"))
+Hindi = int(input(" Hind:"))
+
+# Caculating the percentage of the marks obtained
+print(" The percentage of the marks obtained: ", ((Maths + English + Science + Hindi)/400)*100) 
