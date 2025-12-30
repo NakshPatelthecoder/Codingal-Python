152dd0ae52ae037f8e6b7d7557cc6661a001e60e
nakshpatelthecoder <25pateln@students.watfordboys.org>
2025-12-30 17:43:37 +0000

    5.ACT4.py

---
+++ Lesson5/5.ACT4.py
@@ -0,0 +1,7 @@
+number = int(input(" Enter a number to see wheter it is odd or even: "))
+print (" Number to be checked : ", number)
+
+if number%2==0:
+    print (" This number is an even number. ")
+else:
+    print (" Thsi number is an odd number. ")
