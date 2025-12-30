96b05f54b9e12fcb01c738c568a69b3d370550de
nakshpatelthecoder <25pateln@students.watfordboys.org>
2025-12-30 17:37:44 +0000

    5.ACT3.py

---
+++ Lesson5/5.ACT3.py
@@ -0,0 +1,8 @@
+i = int(input(" Please enter a number: "))
+if (i < 15):
+    print (" i is smaller than 15 ")
+    print (" I'm in the IF Block ")
+else:
+    print (" i is greater than 15 ")
+    print (" I'm in the ELSE Block")
+print (" I'm not in the IF or ELSE Block ")
