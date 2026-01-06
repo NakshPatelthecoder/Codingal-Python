4b091042e90be4967af49ad6a26aedd2cfa6bd15
nakshpatelthecoder <25pateln@students.watfordboys.org>
2026-01-06 17:41:26 +0000

    6.ACT2.py

---
+++ Lesson6/6.ACT2.py
@@ -0,0 +1,8 @@
+print(6!=7)
+num1 = int(input(" Please enter your first number. "))
+num2 = int(input(" Please enter second number. "))
+
+if num1 != num2:
+    print (" Both numbers are different. ")
+else:
+    print (" Both numbers are the same. ")
