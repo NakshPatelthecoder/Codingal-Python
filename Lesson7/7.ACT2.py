bf6dfda0fed532240a09a52e393ec276477c54e5
nakshpatelthecoder <25pateln@students.watfordboys.org>
2026-01-13 17:46:01 +0000

    7.ACT3.py

---
+++ Lesson7/7.ACT2.py
@@ -0,0 +1,13 @@
+a = int(input(" Please enter your first number. "))
+b = int(input(" Please enter your second number. "))
+
+# Printing the bitwise right shift operator
+print (" a >> 1 =", a >> 1 )
+print (" b >> 1 ", b >> 1 )
+
+a = int(input(" Please enter your first number. "))
+b = int(input(" Please enter your second number. "))
+
+# Printing the bitwise left shift operator
+print (" a << 1 =", a << 1 )
+print (" b << 1 ", b << 1 )
