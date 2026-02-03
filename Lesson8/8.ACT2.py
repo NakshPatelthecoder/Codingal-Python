c6a8a288a18e1ef4d6c7c1f975cc90b9fe4e3aba
nakshpatelthecoder <25pateln@students.watfordboys.org>
2026-02-03 17:28:02 +0000

    8.ACT2.py

---
+++ Lesson8/8.ACT2.py
@@ -0,0 +1,7 @@
+numn = int(input("Enter a numerator "))
+numd = int(input("Enter a denominator "))
+
+if numn%numd==0:
+    print ("\n" +str(numn)+  " is divisble by "  +str(numd))
+else:
+    print("\n" +str(numn)+ "is NOT divisble by" +str(numd))
