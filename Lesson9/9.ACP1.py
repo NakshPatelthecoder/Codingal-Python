bc2e8344fb9140cb762cdd7ccc8bbfc6d2504bcb
nakshpatelthecoder <25pateln@students.watfordboys.org>
2026-02-15 10:31:10 +0000

    9.ACP1.py

---
+++ Lesson9/9.ACP1.py
@@ -0,0 +1,8 @@
+# Program to check if age is between 10 and 20
+
+age = int(input("Enter your age: "))
+
+if 10 <= age <= 20:
+    print("Your age is between 10 and 20.")
+else:
+    print("Your age is NOT between 10 and 20.")
