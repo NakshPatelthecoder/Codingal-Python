7559356794c248fec631cab975e3c74ef800bb4c
nakshpatelthecoder <25pateln@students.watfordboys.org>
2026-02-23 17:49:27 +0000

    10.ACT2.py

---
+++ Module2/Lesson10/10.ACT2.py
@@ -0,0 +1,8 @@
+string1 = input(" Please enter your own String: ")
+
+string2 = ''
+for i in string1:
+    string2 = i + string2
+
+print (" The Original String: ", string1)
+print (" The Reversed String: ", string2)
