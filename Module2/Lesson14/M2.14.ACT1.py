5bac5ec2080dba091a01bf32fad19c0aa0ae602b
nakshpatelthecoder <25pateln@students.watfordboys.org>
2026-03-23 18:09:53 +0000

    M2.14.ACT1.py

--- Module2/Lesson14/M2.14.ACT1.py
+++ Module2/Lesson14/M2.14.ACT1.py
@@ -1,10 +1,10 @@
 import turtle   # Importing Library
-turtle.Screen().bgcolor("Orange")
+turtle.Screen().bgcolor(input(" What colour do you want the background to be ( It must be spelt correctly): "))
 turtle.Screen().setup(300,400)
 polygon = turtle.Turtle() # defined variable
 
 num_sides = int(input(" Enter a number of sides for your polygon (Must be 3 or more): "))
-side_length = 70
+side_length = int(input(" Enter a number between 40 - 200 "))
 angle = 360.0 / num_sides
 # Iterate loop for total number of sides
 for i in range(num_sides):
