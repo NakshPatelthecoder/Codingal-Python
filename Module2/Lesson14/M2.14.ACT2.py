fcde4cefa9fb8f2a5a0e413964f616a28915b8e6
nakshpatelthecoder <25pateln@students.watfordboys.org>
2026-03-23 17:53:48 +0000

    M2.14.ACT2.py

---
+++ Module2/Lesson14/M2.14.ACT2.py
@@ -0,0 +1,32 @@
+import turtle
+
+turtle.Screen().bgcolor("Aqua")
+board = turtle.Turtle()
+
+#First Triangle
+
+board.forward(100) # Drawing the base
+
+board.left(120)
+board.forward(100)
+
+board.left(120)
+board.forward(100)
+
+board.penup()
+board.right(150)
+board.forward(50)
+
+# Second Triangle 
+
+board.pendown()
+board.right(90)
+board.forward(100)
+
+board.right(120)
+board.forward(100)
+
+board.right(120)
+board.forward(100)
+
+turtle.done()
