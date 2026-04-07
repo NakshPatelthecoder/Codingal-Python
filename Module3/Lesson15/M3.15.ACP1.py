1ba69fee5e79eeaddd96330284b0900f48d1842c
nakshpatelthecoder <25pateln@students.watfordboys.org>
2026-04-07 13:19:57 +0100

    M3.15.ACP1.py

---
+++ Module3/Lesson15/M3.15.ACP1.py
@@ -0,0 +1,11 @@
+# Ask user for radius
+radius = float(input("Enter the radius of the circle: "))
+
+# Use an approximate value for pi
+pi = 3.14159
+
+# Calculate circumference
+circumference = 2 * pi * radius
+
+# Display result
+print("The circumference of the circle is:", circumference)
