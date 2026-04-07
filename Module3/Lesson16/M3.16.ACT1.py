caa9cabd1595eb414c976b0c870af14f4c9d548e
nakshpatelthecoder <25pateln@students.watfordboys.org>
2026-04-07 17:48:33 +0100

    M3.1.ACT1.py

---
+++ Module3/Lesson16/M3.16.ACT1.py
@@ -0,0 +1,7 @@
+def total_calc(bill_amount,tip_perc):
+    total = bill_amount*(1+0.01*tip_perc)
+    total = round(total,2)
+    print (f" Please pay ${total} ")
+
+
+total_calc ((int(input(" Please enter amount of the total bill that you have received. "))),(int(input(" Please enter the tip that you would like to pay as a percentage. ")))) 
