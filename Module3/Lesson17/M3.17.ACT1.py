0f66366de6246b3c3ce32bb8eb98e046dec10656
nakshpatelthecoder <25pateln@students.watfordboys.org>
2026-04-14 17:51:47 +0100

    M3.17.ACT1.py

---
+++ Module3/Lesson17/M3.17.ACT1.py
@@ -0,0 +1,7 @@
+a = input(" Enter a word. ")
+for i in a:
+    if (i=='A'):
+        print(" The letter A has been found in ", a)
+        break
+    else:
+        print (" The letter A has NOT been found in  ", a)
