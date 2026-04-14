fba8a1b5aa29aca29a9c120edfbedb1af730357c
nakshpatelthecoder <25pateln@students.watfordboys.org>
2026-04-14 18:18:34 +0100

    M3.17.ACT3.py

--- Module3/Lesson17/M3.17.ACT3.py
+++ Module3/Lesson17/M3.17.ACT3.py
@@ -1,10 +1,10 @@
 var = int(input(" Please enter a number that is less than 100: "))
 while var>100:
     var = int(input(" Please enter a number that is less than 100: "))
-
-while var>0:
+num =int(input(" Enter a number that you want to skip "))
+while var > 0:
     var -= 1
-    if var == 5:
+    if var == num:
         continue
     print(" Current variable value : ", var)
 print ("  \n Goodye and we hope to see you again!")
