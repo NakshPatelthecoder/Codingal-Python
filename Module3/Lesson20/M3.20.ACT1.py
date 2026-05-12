2c1b6a8387c342458c9e16f4b7879ec1b0433415
nakshpatelthecoder <25pateln@students.watfordboys.org>
2026-05-12 17:40:31 +0100

    M3.20.ACT1.py

---
+++ Module3/Lesson20/M3.20.ACT1.py
@@ -0,0 +1,9 @@
+from datetime import date , time , datetime
+
+today = date.today()
+now = datetime.now()
+print (" Today's date is  ", today)
+print (" \n The Current Date and Time is  ", now)
+
+
+print (" \n Date components ", today.year, today.month, today.day)
