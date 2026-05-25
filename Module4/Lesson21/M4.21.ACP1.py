032417286a9698261edc4aae4c5d8fe419ef7084
nakshpatelthecoder <25pateln@students.watfordboys.org>
2026-05-25 15:13:22 +0100

    M4.21

--- Module4/Lesson21/M4.21.ACP1.py
+++ Module4/Lesson21/M4.21.ACP1.py
@@ -1,18 +1,6 @@
-# Square it Out!
-
-# Ask the user for the start and end numbers
-start = int(input("Enter the starting number: "))
-end = int(input("Enter the ending number: "))
-
-# Create a list of square values
-squares = []
-
-for num in range(start, end + 1):
-    squares.append(num ** 2)
-
-# Separate odd and even square values
 even_squares = []
 odd_squares = []
+
 
 for square in squares:
     if square % 2 == 0:
@@ -20,12 +8,15 @@ for square in squares:
     else:
         odd_squares.append(square)
 
+
 # Display the results
 print("\nAll square values:")
 print(squares)
 
+
 print("\nEven square values:")
 print(even_squares)
+
 
 print("\nOdd square values:")
 print(odd_squares)
--- Module4/Lesson21/M4.21.ACT3.py
+++ Module4/Lesson21/M4.21.ACT3.py
@@ -1,8 +1,15 @@
 L = [4, 5, 1, 2, 9, 7, 10, 8]
 print(" The original list : ", L)
 
+
 print(" The sum of the list is: ",sum(L))
 print(" The average of the list is: ",sum(L)/len(L))
 
+
 print(" The snallest element is: ", min(L))
 print(" The largest element is: ", max(L))
+
+
+
+
+
