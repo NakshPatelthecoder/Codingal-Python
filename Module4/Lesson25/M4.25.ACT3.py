9c489684a5c370350701def154ccba274b0bd771
nakshpatelthecoder <25pateln@students.watfordboys.org>
2026-06-16 18:06:52 +0100

    M4.25.ACT3.py

---
+++ Module4/Lesson25/M4.25.ACT3.py
@@ -0,0 +1,8 @@
+num = int(input("Enter a number that you want to operate on the basis of:"))
+
+for i in range (9999999999999999999999999999999999999999999999999999999):
+
+    if i == num:
+        print("The number has been located\n You are now exiting the program")
+        exit()
+    print(i)
