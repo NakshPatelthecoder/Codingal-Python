af13e6786ffaebe1dab7a44d34ffa71fe1cf3b44
nakshpatelthecoder <25pateln@students.watfordboys.org>
2026-06-23 18:19:52 +0100

    DataStructure-PCT3

---
+++ Module4/DataStructurePCT3/GradeBook.py
@@ -0,0 +1,20 @@
+grades = {
+        "Alice":95,
+        "Bob":90,
+        "Charlie":54,
+        "Devon":98,
+        "Enoch":3
+}
+
+average = sum(grades.values()) / 5
+print(average)
+
+
+#What you need to use
+#3.  max() min()     →  to find the top and bottom scorer
+#4.  .get()          →  to look up a student by name
+#5.  input()         →  to let the user search for a student
+#What you'll be marked on
+#3.  Highest and lowest scores and students identified             →  10 marks
+#4.  .get() used to look up student — friendly message if missing  →  10 marks
+#5.  Program runs without any errors                               →   5 marks
