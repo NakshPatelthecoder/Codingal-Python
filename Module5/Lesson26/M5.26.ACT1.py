297e4dcb939701d26ef1b94e428ab032fdb55d3d
nakshpatelthecoder <25pateln@students.watfordboys.org>
2026-07-07 17:46:45 +0100

    M5.26.ACT1.py

---
+++ Module5/Lesson26/M5.26.ACT1.py
@@ -0,0 +1,10 @@
+class student:
+    grade = 10
+    print("Hi. I in the 10th grade.")
+    def __init__(self):
+        print("I have automatically been summoned.")
+    def hello(self):
+        print("Hello self.")
+
+s=student()
+s.hello()
